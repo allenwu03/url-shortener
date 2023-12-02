5405b5c7a0e3e49a4e5a5fccc2f00577731844b5
Allen <allenwu032016@gmail.com>
2023-12-02 21:55:57 +0800

    feat: add create url function

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const URL = require('./models/url')
+const shortenURL = require('./utils/shortenURL')
+const bodyParser = require('body-parser')
 
 const app = express()
 const port = 3000
@@ -8,6 +11,11 @@ const port = 3000
 if (process.env.NODE_ENV != 'production') {
   require('dotenv').config()
 }
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
@@ -23,12 +31,42 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
-app.set('view engine', 'hbs')
-app.use(express.static('public'))
-
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.post('/', (req, res) => {
+  if (!req.body.url) {
+    return res.redirect('/')
+  }
+
+  const shortURL = shortenURL(5)
+  URL.findOne({ originalURL: req.body.url })
+    .then((data) =>
+      data ? data : URL.create({ shortURL, originURL: req.body.url })
+    )
+    .then((data) =>
+      res.render('index', {
+        origin: req.headers.origin,
+        shortURL: data.shortURL
+      })
+    )
+    .catch((error) => console.error(error))
+})
+
+app.get('/:shortURL', (req, res) => {
+  const { shortURL } = req.params
+  URL.findOne({ shortURL })
+    .then((data) => {
+      if (!data) {
+        return res.render('error', {
+          errorMsg: "Can't found the URL",
+          errorURL: req.header.host + '/' + shortURL
+        })
+      }
+      res.redirect(data.originalURL)
+    })
+    .catch((error) => console.error(error))
 })
 
 app.listen(port, () => {
