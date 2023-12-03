93d74742319326fd235d2a9551a3161282bed0d8
Allen <allenwu032016@gmail.com>
2023-12-03 16:16:50 +0800

    feat: modify style.css

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+require('./config/mongoose')
 const URL = require('./models/url')
 const shortenURL = require('./utils/shortenURL')
 const bodyParser = require('body-parser')
@@ -8,28 +8,10 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
-if (process.env.NODE_ENV != 'production') {
-  require('dotenv').config()
-}
-
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
-
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.get('/', (req, res) => {
   res.render('index')
